82a5fbbcde0515aeb4d860d57f907351dfa95fdf
Swagnik Dutta <swagnikd@gmail.com>
2023-09-05 18:48:06 +0530

    refactor: abstracts network address resolution and network operations
    This commit abstracts address resolution logic and related network
    operations behind interfaces. This refactoring simplifies unit testing
    by encapsulating side-effect-inducing code behind interfaces,
    improving code maintainability and testability.

--- pkg/ping/ping.go
+++ pkg/ping/ping.go
@@ -18,92 +18,12 @@ var (
 	ICMPHeaderSubtype  uint8 = 0
 )
 
-func (h *ICMPHeader) Serialize() ([]byte, error) {
-	buf := new(bytes.Buffer)
-
-	if err := binary.Write(buf, binary.BigEndian, h.Type); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.Code); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.Checksum); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.Identifier); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.SequenceNumber); err != nil {
-		return nil, err
-	}
-
-	return buf.Bytes(), nil
-}
-
-func (p *ICMPPacket) Serialize() ([]byte, error) {
-	return p.Header.Serialize()
-}
-
-func (h *IPv4Header) Serialize() ([]byte, error) {
-	buf := new(bytes.Buffer)
-	if err := binary.Write(buf, binary.BigEndian, h.Version); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.IHL); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.TypeOfService); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.TotalLength); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.Identification); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.Flags); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.FragmentOffset); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.TTL); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.Protocol); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.Checksum); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.SourceIP); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.BigEndian, h.DestinationIP); err != nil {
-		return nil, err
-	}
-
-	return buf.Bytes(), nil
-}
-
-func (p *IPv4Packet) Serialize() ([]byte, error) {
-	buf := new(bytes.Buffer)
-
-	payloadSerialized, err := p.Payload.Serialize()
-	if err != nil {
-		return nil, errors.Wrapf(err, "error serializing ICMP packet")
-	}
-
-	buf.Write(payloadSerialized)
-
-	headerSerialized, err := p.Header.Serialize()
-	if err != nil {
-		return nil, errors.Wrapf(err, "error serializing IPv4 packet header")
-	}
-
-	buf.Write(headerSerialized)
-
-	return buf.Bytes(), nil
+type Pinger struct {
+	sourceIP net.IP
+	destIP   net.IP
+	count    uint8
+	resolver AddressResolver
+	dialer   NetworkDialer
 }
 
 func (pinger *Pinger) createICMPPacket(seqNo int) (*ICMPPacket, error) {
@@ -159,31 +79,6 @@ func (pinger *Pinger) createIPv4Packet(count int) (*IPv4Packet, error) {
 	return ipPacket, nil
 }
 
-func (pinger *Pinger) resolveAddress(dest string) error {
-	ips, err := net.LookupIP(dest)
-	if err != nil {
-		return errors.Wrapf(err, "error resolving address of remote host")
-	}
-
-	for _, ip := range ips {
-		if ipv4 := ip.To4(); ipv4 != nil {
-			pinger.destIP = ipv4
-		}
-	}
-
-	// The destination address does not need to exist as unlike tcp, udp does not require a handshake.
-	// The goal here is to retrieve the outbound IP. Source: https://stackoverflow.com/a/37382208/3728336
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		return errors.Wrapf(err, "error resolving outbound ip address of local machine")
-	}
-	defer conn.Close()
-
-	pinger.sourceIP = conn.LocalAddr().(*net.UDPAddr).IP
-
-	return nil
-}
-
 func (pinger *Pinger) parseEchoReply(echoReply []byte, echoRequest *IPv4Packet) {
 	icmpOffset := 20
 	icmpHeaderSize := 8
@@ -191,15 +86,23 @@ func (pinger *Pinger) parseEchoReply(echoReply []byte, echoRequest *IPv4Packet) 
 
 	seqNoOffset := 6
 	seqNo := binary.BigEndian.Uint16(icmpHeaderBytes[seqNoOffset:])
-	fmt.Printf("received ICMP echo packet from %v, seq no: %v\n\n", echoRequest.Header.SourceIP, seqNo)
+	fmt.Printf("received ICMP echo packet from %v, seq no: %v\n\n", echoRequest.Header.DestinationIP, seqNo)
 }
 
 func (pinger *Pinger) Ping(host string) error {
 	log.Printf("Performing ping tests...\n\n")
 
-	if err := pinger.resolveAddress(host); err != nil {
-		return errors.Wrapf(err, "error resolving source/destination addresses")
+	ip, err := pinger.resolver.ResolveSource()
+	if err != nil {
+		return errors.Wrapf(err, "error resolving source address")
 	}
+	pinger.sourceIP = ip
+
+	ip, err = pinger.resolver.ResolveDestination(host)
+	if err != nil {
+		return errors.Wrapf(err, "error resolving destination address")
+	}
+	pinger.destIP = ip
 
 	for i := 0; i < int(pinger.count); i++ {
 		packet, err := pinger.createIPv4Packet(i)
@@ -211,9 +114,8 @@ func (pinger *Pinger) Ping(host string) error {
 		if err != nil {
 			return errors.Wrapf(err, "error serializing IPv4 packet")
 		}
-		// pinger.printSerializedData(packetSerialized, "echo request ip packet(payload|header)")
 
-		conn, err := net.Dial("ip4:icmp", packet.Header.DestinationIP.String())
+		conn, err := pinger.dialer.Dial("ip4:icmp", packet.Header.DestinationIP.String())
 		if err != nil {
 			return errors.Wrapf(err, "error eshtablishing connection with %s", host)
 		}
@@ -223,7 +125,8 @@ func (pinger *Pinger) Ping(host string) error {
 		if err != nil {
 			return errors.Wrapf(err, "error sending ICMP echo request")
 		}
-		fmt.Printf("sending ICMP echo request (%v bytes) from %v, seq_no: %v\n", packet.Header.TotalLength, packet.Header.SourceIP, packet.Payload.Header.SequenceNumber)
+		fmt.Printf("sending ICMP echo request (%v bytes) from %v, to %v, seq_no: %v\n", packet.Header.TotalLength,
+			packet.Header.SourceIP, packet.Header.DestinationIP, packet.Payload.Header.SequenceNumber)
 
 		echoReply := make([]byte, 2048)
 		_, err = conn.Read(echoReply)
@@ -240,8 +143,11 @@ func (pinger *Pinger) Ping(host string) error {
 }
 
 func NewPinger() *Pinger {
-	pinger := new(Pinger)
-	pinger.count = 3
+	pinger := &Pinger{
+		count:    3,
+		resolver: new(LocalResolver),
+		dialer:   new(Dialer),
+	}
 
 	return pinger
 }
