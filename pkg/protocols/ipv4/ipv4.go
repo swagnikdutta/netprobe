3bf514f754ece7b86ab385c1fe230f5ca4e06385
Swagnik Dutta <swagnikd@gmail.com>
2024-04-04 13:04:28 +0530

    refactor: moved defer statement within a function

--- pkg/protocols/ipv4/ipv4.go
+++ pkg/protocols/ipv4/ipv4.go
@@ -1,6 +1,8 @@
 package ipv4
 
 import (
+	"net"
+
 	"github.com/pkg/errors"
 	"github.com/swagnikdutta/netprobe/pkg/protocols"
 )
@@ -14,15 +16,35 @@ func calculateTotalLength(p *Packet) (*uint16, error) {
 	return &length, nil
 }
 
-func CreatePacket(h Header, payload []byte) (*Packet, []byte, error) {
+func CreatePacket(
+	version uint8,
+	ihl uint8,
+	tos uint8,
+	len uint16,
+	id uint16,
+	flag uint8,
+	fo uint16,
+	ttl uint8,
+	proto uint8,
+	ch uint16,
+	src net.IP,
+	dest net.IP,
+	payload []byte,
+) (*Packet, []byte, error) {
 	p := &Packet{
 		Header: &Header{
-			Version:       h.Version,
-			IHL:           h.IHL,
-			TTL:           h.TTL,
-			Protocol:      h.Protocol,
-			SourceIP:      h.SourceIP,
-			DestinationIP: h.DestinationIP,
+			Version:        version,
+			IHL:            ihl,
+			TypeOfService:  tos,
+			TotalLength:    len,
+			Identification: id,
+			Flags:          flag,
+			FragmentOffset: fo,
+			TTL:            ttl,
+			Protocol:       proto,
+			Checksum:       ch,
+			SourceIP:       src,
+			DestinationIP:  dest,
 		},
 		Payload: payload,
 	}
